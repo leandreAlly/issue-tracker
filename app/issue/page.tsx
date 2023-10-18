e18939d1493bc0083363aab0b91f8b3043564609
leandre <tuyambazeleandre@gmail.com>
2023-10-18 09:14:28 +0200

    Build the Issue the page

--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button>new Issue</Button>
+      <Button>
+        <Link href="/issue/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
