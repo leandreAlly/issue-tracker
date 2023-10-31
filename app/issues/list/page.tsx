4ed2e0a4b1dd835618eeda62d638df36639fca1b
leandre <tuyambazeleandre@gmail.com>
2023-10-31 12:24:14 +0200

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -11,17 +11,6 @@ interface Props {
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
-
-  const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
-  });
-
   const columns: {
     label: string;
     value: keyof Issue;
@@ -31,6 +20,24 @@ const IssuesPage = async ({ searchParams }: Props) => {
     { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
     { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
   ];
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+    orderBy,
+  });
 
   return (
     <div>
